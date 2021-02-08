8c3a79c2e2dffb6cdd1014511662120a4dc5307d
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-02-08 18:25:50 -0400

    Creando entidad Ciudades

---
+++ -Parcial1-ap1--20180616/Entidades/Ciudades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace _Parcial1_ap1__20180616.Entidades
+{
+    public class Ciudades
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        public string CiudadNombre { get; set; }
+    }
+}
