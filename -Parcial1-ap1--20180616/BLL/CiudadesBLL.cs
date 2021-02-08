b842db896881d31546c60b3cf2e6f849092d1c45
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-02-08 18:59:57 -0400

    Creando carpeta BLL

---
+++ -Parcial1-ap1--20180616/BLL/CiudadesBLL.cs
@@ -0,0 +1,139 @@
+﻿using _Parcial1_ap1__20180616.DAL;
+using _Parcial1_ap1__20180616.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _Parcial1_ap1__20180616.BLL
+{
+    public class CiudadesBLL
+    {
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Ciudades.Any(e => e.CiudadId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        public static bool Insertar(Ciudades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Ciudades.Add(ciudades);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Ciudades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(ciudades).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Guardar(Ciudades ciudades)
+        {
+            if (!Existe(ciudades.CiudadId))
+                return Insertar(ciudades);
+            else
+                return Modificar(ciudades);
+        }
+
+        public static Ciudades Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Ciudades ciudades;
+
+            try
+            {
+                ciudades = contexto.Ciudades.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ciudades;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var ciudades = contexto.Ciudades.Find(id);
+                
+                if(ciudades != null)
+                {
+                    contexto.Ciudades.Remove(ciudades);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+    }
+}
