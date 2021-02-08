9ee99f6e7947071489ab5a85a985b63d823a3155
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-02-08 18:36:57 -0400

    Creando caperta DAL

---
+++ -Parcial1-ap1--20180616/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using _Parcial1_ap1__20180616.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _Parcial1_ap1__20180616.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Ciudades> Ciudades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = GestionCiudades.Db");
+        }
+    }
+}
